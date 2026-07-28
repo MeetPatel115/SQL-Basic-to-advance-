760d34180192f7b4706d088bfb96bc4ea8371aed
MeetPatel115 <mb115patel@gmail.com>
2026-07-28 18:20:24 +0530

    THHI IS FOR THE UNION OF TABLES

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -1,1 +1,19 @@
-select * from customers
+SELECT * 
+FROM Sales.Customers;
+
+
+SELECT *
+FROM Sales.Employees;
+
+
+-- combine all the  dataa from the employees and customers tables 
+/* UNION */
+
+SELECT FirstName,
+LastName
+FROM Sales.Customers
+UNION
+SELECT FirstName,
+LastName
+FROM Sales.Employees;
+
