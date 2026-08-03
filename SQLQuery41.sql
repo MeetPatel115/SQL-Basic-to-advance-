639e90f14b1b6c9eb9195e6997409b5ae1b4fbb8
MeetPatel115 <mb115patel@gmail.com>
2026-08-03 23:14:30 +0530

    AGGREGATED

--- SQLQuery41.sql
+++ SQLQuery41.sql
@@ -2,3 +2,9 @@ Select count(*) as num_of_order,
 Datename(MONTH,CreationTime) as month_s 
 from Sales.Orders
 Group by Datename(MONTH,CreationTime)
+
+
+Select count(*) as num_of_order,
+Datename(MONTH,CreationTime) as month_s 
+from Sales.Orders
+Group by Datename(MONTH,CreationTime)
